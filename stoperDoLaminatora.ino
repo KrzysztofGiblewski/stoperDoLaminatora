f7b1bfd614cf97b5f7b6988bc8213ccd889d81a2
KrzysztofGiblewski <krzysztofgiblewski@gmail.com>
2020-04-20 14:58:39 +0200

    Działa stoper w stopniu podstawowym

--- stoperDoLaminatora.ino
+++ stoperDoLaminatora.ino
@@ -1,47 +1,57 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+
 unsigned long poczatekMilisekund = 0;
 unsigned long koniecMilisekund = 0;
 int ekrany = 0;
-//LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Ustawienie adresu ukladu na 0x27         A4 SDA        A5 SCL
-LiquidCrystal_I2C lcd(0x27,20,4);
+boolean mierzCzas = false;
+boolean zmierzone = false;
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
 void setup() {
-   lcd.init();                      // initialize the lcd 
+  lcd.init();                      // initialize the lcd
   lcd.init();
-   lcd.backlight();
-  lcd.setCursor(3,0);
-   Serial.begin(9600);
+  lcd.backlight();
+  lcd.setCursor(3, 0);
+  Serial.begin(9600);
   pinMode(A0, INPUT_PULLUP);                //przycisk dodawania sztuki A0    -   lewy
   pinMode(A1, INPUT_PULLUP);                // przycisk odejmowania A1      - środkowy
   pinMode(A2, INPUT_PULLUP);                //przycisk wyboru A2            -prawy
-  pinMode(A3, OUTPUT); //Konfiguracja A3 jako wyjście dla buzzera
-  
+  // pinMode(A3, OUTPUT); //Konfiguracja A3 jako wyjście dla buzzera
+
   pinMode(A6, OUTPUT);       //przekaznik1 jako wyjście
 
 }
 
 void loop() {
   wyswietl();
-//  buzerr();
+  //  buzerr();
 }
 void wyswietl() {
-  poczatekMilisekund = millis();
+  // poczatekMilisekund = millis();
   pierwszaLinia(millis());
   switch (ekrany)
   {
     case 0:             {
         if (digitalRead(A0) == LOW)   {
-          koniecMilisekund=millis()-poczatekMilisekund;
-          drugaLinia("po ", poczatekMilisekund, " ko ", koniecMilisekund);
+          mierzCzas = true;
+          zmierzone = false;
+          poczatekMilisekund = millis();
+          drugaLinia("Start ", 0, "  STOP  ", 0);
         }
         if (digitalRead(A1) == LOW)   {
-        drugaLinia("po ", poczatekMilisekund, " ko ", koniecMilisekund);
+          mierzCzas = false;
+          zmierzone = true;
+          koniecMilisekund = millis() - poczatekMilisekund;
+          drugaLinia("Stop              ",  koniecMilisekund / 1000, ",", koniecMilisekund % 1000);
+          drugaLinia("S ",  koniecMilisekund / 1000, ",", koniecMilisekund % 1000);
         }
         break;
       }
     case 1:              {
         if (digitalRead(A0) == LOW)   {
-          
+
         }
         if (digitalRead(A1) == LOW)   {
         }
@@ -50,14 +60,21 @@ void wyswietl() {
 
 
   }
-
+  if (mierzCzas == true) {
+    koniecMilisekund = millis() - poczatekMilisekund;
+    drugaLinia("S ",  koniecMilisekund / 1000, ",", koniecMilisekund % 1000);
+  }
+  if (zmierzone == true && mierzCzas == false)
+    drugaLinia("S ",  koniecMilisekund / 1000, ",", koniecMilisekund % 1000);
+  if (zmierzone == false && mierzCzas == false)
+    drugaLinia("START ",  0, " STOP ", 0);
 }
 void zmienEkrany() {
   ekrany++;
   delay(100);
-  if (ekrany>3)
-  ekrany=0;
- }
+  if (ekrany > 1)
+    ekrany = 0;
+}
 void pierwszaLinia(unsigned long milisekundy) {
   lcd.setCursor(0, 0);
   lcd.print(milisekundy);
